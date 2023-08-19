85430e24253b87a9298bfa13df4cfd415e479d85
YoungHyunKoo <kooala317@gmail.com>
2023-08-19 12:59:35 -0400

    add scheduler

--- Train_torch_Unet_distributed2.py
+++ Train_torch_Unet_distributed2.py
@@ -15,6 +15,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.optim.lr_scheduler import ExponentialLR
+
 import torch.distributed as dist
 from torch.utils import collect_env
 from torch.utils.data import TensorDataset
