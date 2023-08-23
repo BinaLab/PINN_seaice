97be9ceb55d3a475b7c9c6173a50333f7d6ad58c
YoungHyunKoo <kooala317@gmail.com>
2023-08-22 23:41:23 -0400

    updates

--- Train_torch_Unet_distributed2.py
+++ Train_torch_Unet_distributed2.py
@@ -503,9 +503,9 @@ def main() -> None:
     )
 
     if out_channels == 2:
-        model_name = f"torch_cnn_uv_lr{lr}_wo{date}_{phy}_day{dayint}_{device_name}{world_size}"
+        model_name = f"torch_unet_uv_lr{lr}_wo{date}_{phy}_day{dayint}_{device_name}{world_size}"
     else:
-        model_name = f"torch_cnn_sit_lr{lr}_wo{date}_{phy}_day{dayint}_{device_name}{world_size}"
+        model_name = f"torch_unet_sit_lr{lr}_wo{date}_{phy}_day{dayint}_{device_name}{world_size}"
 
     if phy == "phy":
         loss_fn = physics_loss() # nn.L1Loss() #nn.CrossEntropyLoss()
