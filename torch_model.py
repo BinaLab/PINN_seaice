851833738907ccbf2d17b0286326609cf83e6bc8
YoungHyunKoo <kooala317@gmail.com>
2023-08-31 22:20:28 -0400

    update

--- torch_model.py
+++ torch_model.py
@@ -764,7 +764,7 @@ class UNet(nn.Module):
     def __init__(self, n_inputs, n_outputs, k=3):
         super().__init__()
         
-        self.activation = nn.ReLU()
+        self.activation = nn.LeakyReLU(negative_slope=1.0)
         
         # Encoder
         # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
