0d009e3518e3ae5e7ea90de850290026bf5fd249
YoungHyunKoo <kooala317@gmail.com>
2023-09-16 01:16:08 -0400

    model updates with simplication

--- torch_model.py
+++ torch_model.py
@@ -1238,7 +1238,209 @@ class TS_UNet(nn.Module):
 
         return out
     
-# UNET model
+# Early branch UNET model
+class EB_UNet(nn.Module):
+    def __init__(self, n_inputs, n_outputs, k=3):
+        super().__init__()
+        
+        self.activation = nn.Tanh()
+        
+        self.first_conv = nn.Conv2d(n_inputs, 32, kernel_size=k, padding="same")
+        
+        ##### SIU BRANCH #####
+        # input: 320x320x64
+        self.siu_ec1 = encoder(32, 64) # output: 160x160x64
+        # input: 160x160x64
+        self.siu_ec2 = encoder(64, 128) # output: 80x80x128
+        # input: 80x80x128
+        self.siu_ec3 = encoder(128, 256) # output: 40x40x256
+
+        # input: 40x40x256
+        self.siu_ec41 = nn.Conv2d(256, 512, kernel_size=k, padding="same") # output: 40x40x512
+        self.siu_ec42 = nn.Conv2d(512, 512, kernel_size=k, padding="same") # output: 40x40x512
+
+        # Decoder
+        self.siu_dc1 = decoder(512, 256) # output: 80x80x256
+        self.siu_dc2 = decoder(256, 128) # output: 160x160x128
+        self.siu_dc3 = decoder(128, 64) # output: 320x320x64     
+        
+        ##### SIV BRANCH #####
+        # input: 320x320x64
+        self.siv_ec1 = encoder(32, 64) # output: 160x160x64
+        # input: 160x160x64
+        self.siv_ec2 = encoder(64, 128) # output: 80x80x128
+        # input: 80x80x128
+        self.siv_ec3 = encoder(128, 256) # output: 40x40x256
+
+        # input: 40x40x256
+        self.siv_ec41 = nn.Conv2d(256, 512, kernel_size=k, padding="same") # output: 40x40x512
+        self.siv_ec42 = nn.Conv2d(512, 512, kernel_size=k, padding="same") # output: 40x40x512
+
+        # Decoder
+        self.siv_dc1 = decoder(512, 256) # output: 80x80x256
+        self.siv_dc2 = decoder(256, 128) # output: 160x160x128
+        self.siv_dc3 = decoder(128, 64) # output: 320x320x64 
+        
+        ##### SIC BRANCH #####
+        # input: 320x320x64
+        self.sic_ec1 = encoder(32, 64) # output: 160x160x64
+        # input: 160x160x64
+        self.sic_ec2 = encoder(64, 128) # output: 80x80x128
+        # input: 80x80x128
+        self.sic_ec3 = encoder(128, 256) # output: 40x40x256
+
+        # input: 40x40x256
+        self.sic_ec41 = nn.Conv2d(256, 512, kernel_size=k, padding="same") # output: 40x40x512
+        self.sic_ec42 = nn.Conv2d(512, 512, kernel_size=k, padding="same") # output: 40x40x512
+
+        # Decoder
+        self.sic_dc1 = decoder(512, 256) # output: 80x80x256
+        self.sic_dc2 = decoder(256, 128) # output: 160x160x128
+        self.sic_dc3 = decoder(128, 64) # output: 320x320x64 
+
+        # Output layer
+        self.siu_conv = nn.Conv2d(64, 1, kernel_size=k, padding="same")
+        self.siv_conv = nn.Conv2d(64, 1, kernel_size=k, padding="same")
+        self.sic_conv = nn.Conv2d(64, 1, kernel_size=k, padding="same")
+        
+    def forward(self, x):
+        # First convolution
+        x = self.first_conv(x)        
+        
+        ##### Encoder 1 #####
+        xe1_siu, xe1b_siu = self.siu_ec1(x) # SIU
+        xe1_siv, xe1b_siv = self.siv_ec1(x) # SIV
+        xe1_sic, xe1b_sic = self.sic_ec1(x) # SIC
+        
+        ##### Encoder 2 #####
+        xe2_siu, xe2b_siu = self.siu_ec2(xe1_siu) # SIU
+        xe2_siv, xe2b_siv = self.siv_ec2(xe1_siv) # SIV
+        xe2_sic, xe2b_sic = self.sic_ec2(xe1_sic) # SIC
+
+        ##### Encoder 3 #####
+        xe3_siu, xe3b_siu = self.siu_ec3(xe2_siu) # SIU
+        xe3_siv, xe3b_siv = self.siv_ec3(xe2_siv) # SIV
+        xe3_sic, xe3b_sic = self.sic_ec3(xe2_sic) # SIC
+        
+        ##### Bottom bridge #####
+        # SID
+        xe41_siu = self.activation(self.siu_ec41(xe3_siu))
+        xe42_siu = self.activation(self.siu_ec42(xe41_siu))
+        # SIV
+        xe41_siv = self.activation(self.siv_ec41(xe3_siv))
+        xe42_siv = self.activation(self.siv_ec42(xe41_siv))
+        # SIC
+        xe41_sic = self.activation(self.sic_ec41(xe3_sic))
+        xe42_sic = self.activation(self.sic_ec42(xe41_sic))
+        # output: 40x40x512
+        
+        ##### Decoder 1 #####
+        # SIU
+        xd1_siu = self.siu_dc1(xe42_siu, xe3b_siu)
+        # SIV
+        xd1_siv = self.siv_dc1(xe42_siv, xe3b_siv)
+        # SIC
+        xd1_sic = self.sic_dc1(xe42_sic, xe3b_sic)
+        # Weighting block 5
+        
+        ##### Decoder 2 #####
+        # SIU
+        xd2_siu = self.siu_dc2(xd1_siu, xe2b_siu)
+        # SIV
+        xd2_siv = self.siv_dc2(xd1_siv, xe2b_siv)
+        # SIC
+        xd2_sic = self.sic_dc2(xd1_sic, xe2b_sic)
+        # Weighting block 6        
+        
+        ##### Decoder 3 #####
+        # SIU
+        xd3_siu = self.siu_dc3(xd2_siu, xe1b_siu)
+        # SIV
+        xd3_siv = self.siv_dc3(xd2_siv, xe1b_siv)
+        # SIC
+        xd3_sic = self.sic_dc3(xd2_sic, xe1b_sic)
+
+        siu = self.siu_conv(xd3_siu)
+        siv = self.siv_conv(xd3_siv)
+        sic = self.sic_conv(xd3_sic)
+        
+        out = torch.cat([siu, siv, sic], dim=1)
+
+        return out
+    
+# Early branch UNET model
+class LB_UNet(nn.Module):
+    def __init__(self, n_inputs, n_outputs, k=3):
+        super().__init__()
+        
+        self.activation = nn.Tanh()
+        
+        self.first_conv = nn.Conv2d(n_inputs, 32, kernel_size=k, padding="same")
+        
+        ##### SIU BRANCH #####
+        # input: 320x320x64
+        self.siu_ec1 = encoder(32, 64) # output: 160x160x64
+        # input: 160x160x64
+        self.siu_ec2 = encoder(64, 128) # output: 80x80x128
+        # input: 80x80x128
+        self.siu_ec3 = encoder(128, 256) # output: 40x40x256
+
+        # input: 40x40x256
+        self.siu_ec41 = nn.Conv2d(256, 512, kernel_size=k, padding="same") # output: 40x40x512
+        self.siu_ec42 = nn.Conv2d(512, 512, kernel_size=k, padding="same") # output: 40x40x512
+
+        # Decoder
+        self.siu_dc1 = decoder(512, 256) # output: 80x80x256
+        self.siu_dc2 = decoder(256, 128) # output: 160x160x128
+        self.siu_dc3 = decoder(128, 64) # output: 320x320x64     
+
+        # Output layer
+        self.siu_conv = nn.Conv2d(64, 1, kernel_size=k, padding="same")
+        self.siv_conv = nn.Conv2d(64, 1, kernel_size=k, padding="same")
+        self.sic_conv = nn.Conv2d(64, 1, kernel_size=k, padding="same")
+        
+    def forward(self, x):
+        # First convolution
+        x = self.first_conv(x)        
+        
+        ##### Encoder 1 #####
+        xe1_siu, xe1b_siu = self.siu_ec1(x) # SIU
+        
+        ##### Encoder 2 #####
+        xe2_siu, xe2b_siu = self.siu_ec2(xe1_siu) # SIU
+
+        ##### Encoder 3 #####
+        xe3_siu, xe3b_siu = self.siu_ec3(xe2_siu) # SIU
+        
+        ##### Bottom bridge #####
+        # SID
+        xe41_siu = self.activation(self.siu_ec41(xe3_siu))
+        xe42_siu = self.activation(self.siu_ec42(xe41_siu))
+        # output: 40x40x512
+        
+        ##### Decoder 1 #####
+        # SIU
+        xd1_siu = self.siu_dc1(xe42_siu, xe3b_siu)
+        # Weighting block 5
+        
+        ##### Decoder 2 #####
+        # SIU
+        xd2_siu = self.siu_dc2(xd1_siu, xe2b_siu)
+        # Weighting block 6        
+        
+        ##### Decoder 3 #####
+        # SIU
+        xd3_siu = self.siu_dc3(xd2_siu, xe1b_siu)
+
+        siu = self.siu_conv(xd3_siu)
+        siv = self.siv_conv(xd3_siu)
+        sic = self.sic_conv(xd3_siu)
+        
+        out = torch.cat([siu, siv, sic], dim=1)
+
+        return out
+    
+# Information sharing UNET model
 class IS_UNet(nn.Module):
     def __init__(self, n_inputs, n_outputs, k=3):
         super().__init__()
