d9b731294c8b0e75b46d079b0c156b5b35712199
YoungHyunKoo <kooala317@gmail.com>
2023-09-18 16:15:46 -0400

    updates on loss functions

--- Train_torch_distributed.py
+++ Train_torch_distributed.py
@@ -601,7 +601,7 @@ def main() -> None:
                 loss_fn = single_loss(landmask)
 
     optimizer = optim.Adam(net.parameters(), lr=lr)
-    scheduler = ExponentialLR(optimizer, gamma=0.98)
+    scheduler = ExponentialLR(optimizer, gamma=1.0)
 
     history = {'loss': [], 'val_loss': [], 'time': []}
 
