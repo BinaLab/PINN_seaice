4d649c64e5e26a2913681c2aa7745c5847286911
YoungHyunKoo <kooala317@gmail.com>
2023-09-21 09:08:50 -0400

    update on physics loss function

--- torch_model.py
+++ torch_model.py
@@ -1127,7 +1127,7 @@ class WB(nn.Module):
 class encoder(nn.Module):
     def __init__(self, ch1, ch2, k=3):
         super(encoder,self).__init__()
-        self.activation = nn.LeakyReLU(0.1) #nn.ReLU() #nn.Tanh() #nn.LeakyReLU(0.1)
+        self.activation = nn.ReLU() #nn.ReLU() #nn.Tanh() #nn.LeakyReLU(0.1)
         self.dropout = nn.Dropout(0.3)
         self.e11 = nn.Conv2d(ch1, ch2, kernel_size=k, padding="same") # output: 320x320x64
         self.e12 = nn.Conv2d(ch2, ch2, kernel_size=k, padding="same") # output: 320x320x64
@@ -1143,7 +1143,7 @@ class encoder(nn.Module):
 class decoder(nn.Module):
     def __init__(self, ch1, ch2, k=3):
         super(decoder,self).__init__()
-        self.activation = nn.LeakyReLU(0.1) #nn.ReLU()
+        self.activation = nn.ReLU() #nn.ReLU()
         self.dropout = nn.Dropout(0.3)
         self.upconv1 = nn.ConvTranspose2d(ch1, ch2, kernel_size=2, stride=2) # output: 80x80x256
         self.d11 = nn.Conv2d(ch1, ch2, kernel_size=k, padding="same") # output: 80x80x256
