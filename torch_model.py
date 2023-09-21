7df9f75792834e7844f12489f4bec320dc510482
YoungHyunKoo <kooala317@gmail.com>
2023-09-21 02:05:47 -0400

    update on physics loss function

--- torch_model.py
+++ torch_model.py
@@ -1107,9 +1107,9 @@ class WB(nn.Module):
     def __init__(self, ch, row, col, k=3, w=0.5):
         super(WB,self).__init__()
         self.activation = nn.Tanh()
-        self.a11 = torch.nn.Parameter(torch.ones(ch, row, col)*w)
-        self.a12 = torch.nn.Parameter(torch.ones(ch, row, col)*w)
-        self.a13 = torch.nn.Parameter(torch.ones(ch, row, col)*w)
+        self.a11 = torch.nn.Parameter(torch.ones(row, col)*w)
+        self.a12 = torch.nn.Parameter(torch.ones(row, col)*w)
+        self.a13 = torch.nn.Parameter(torch.ones(row, col)*w)
         self.conv1 = nn.Conv2d(ch, ch, kernel_size=k, padding="same") # output: 160x160x64
         self.conv1 = nn.Conv2d(ch, ch, kernel_size=k, padding="same") # output: 160x160x64
         self.a21 = torch.nn.Parameter(torch.ones(row, col)*w)
