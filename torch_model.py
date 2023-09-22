9bc1d0f971560bb6bff1878b4125c9fa1a53d8bf
YoungHyunKoo <kooala317@gmail.com>
2023-09-22 09:00:04 -0400

    updates

--- torch_model.py
+++ torch_model.py
@@ -176,7 +176,7 @@ class physics_loss(nn.Module):
         # err_phy += 1-r
         # err_phy = torch.mean(torch.where((div > 0) & (d_sic > 0), err_u + err_v + err_sic, 0))
         
-        w = torch.tensor(1000.0)
+        w = torch.tensor(10.0)
         err_sum += w*err_phy
         
         return err_sum    
