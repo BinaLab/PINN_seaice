ae1d83c9db73f5c97620630f0466e84820d636bc
YoungHyunKoo <kooala317@gmail.com>
2023-09-26 02:29:20 -0400

    HIS_Unet update

--- Train_torch_distributed.py
+++ Train_torch_distributed.py
@@ -525,7 +525,7 @@ def main() -> None:
     cnn_output = torch.tensor(cnn_output, dtype=torch.float32)
     
     mask1 = (years == date) # Test samples
-    mask2 = (days % 4 == 2) # Validation samples
+    mask2 = (days % 3 == 2) # Validation samples
 
     val_input = cnn_input[mask1] #cnn_input[(~mask1)&(mask2), :, :, :]
     val_output = cnn_output[mask1] #cnn_output[(~mask1)&(mask2), :, :, :]
