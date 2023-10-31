ca49458aed702640ec2c624d77e719c2eca82ade
YoungHyunKoo <kooala317@gmail.com>
2023-10-31 16:01:32 -0400

    physics loss

--- Train_physics_distributed.py
+++ Train_physics_distributed.py
@@ -647,7 +647,7 @@ def main() -> None:
                 loss_fn = single_loss(landmask)
 
     optimizer = optim.Adam(net.parameters(), lr=lr)
-    scheduler = ExponentialLR(optimizer, gamma=0.95)
+    scheduler = ExponentialLR(optimizer, gamma=0.98)
 
     history = {'loss': [], 'val_loss': [], 'time': []}
 
